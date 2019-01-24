2eb7a050d185268dca3626265b040ff92280825b
Vardan Nadkarni <vardan_nadkarni@persistent.co.in>
2019-01-24 16:14:51 +0530

    Adds API to query loan approved states

--- src/main/kotlin/net/corda/server/controllers/LoanController.kt
+++ src/main/kotlin/net/corda/server/controllers/LoanController.kt
@@ -1,8 +1,13 @@
 package net.corda.server.controllers
 
 import com.template.LoanRequestFlow
+import com.template.LoanSchemaV1
 import com.template.LoanState
 import net.corda.core.contracts.UniqueIdentifier
+import net.corda.core.messaging.vaultQueryBy
+import net.corda.core.node.services.Vault
+import net.corda.core.node.services.vault.QueryCriteria
+import net.corda.core.node.services.vault.builder
 import net.corda.core.utilities.getOrThrow
 import net.corda.server.NodeRPCConnection
 import net.corda.server.models.LoanSimpleObj
@@ -103,5 +108,39 @@ class LoanController(private val rpc: NodeRPCConnection) {
                             state.loanStatus,
                             state.linearId).toSimpleObj()
                 }
+    }
+
+    @GetMapping(value = "/GetApprovedLoans", produces = arrayOf(MediaType.APPLICATION_JSON_VALUE))
+    private fun getApprovedLoans(): List<LoanSimpleObj> {
+
+        val generalCriteria = QueryCriteria.VaultQueryCriteria(Vault.StateStatus.ALL)
+        val results = builder {
+            var loanStatus = LoanSchemaV1.PersistentLoan::loanStatus.equal(true)
+            val customCriteria1 = QueryCriteria.VaultCustomQueryCriteria(loanStatus)
+            var partyType = LoanSchemaV1.PersistentLoan::financeAgency.equal(rpcOps.nodeInfo().legalIdentities.first().name.toString())
+            val customCriteria2 = QueryCriteria.VaultCustomQueryCriteria(partyType)
+            val criteria = generalCriteria.and(customCriteria1).and(customCriteria2)
+            val results = rpcOps.vaultQueryBy<LoanState>(criteria).states
+
+            return results
+                    .map { stateAndRef -> stateAndRef.state.data }
+                    .map { state ->
+                        // We map the anonymous lender and borrower to well-known identities if possible.
+                        val possiblyWellKnownFinanceAgency = rpcOps.wellKnownPartyFromAnonymous(state.financeAgency)
+                                ?: state.financeAgency
+                        val possiblyWellKnownBank = rpcOps.wellKnownPartyFromAnonymous(state.bank) ?: state.bank
+
+                        LoanState(
+                                state.name,
+                                state.amount,
+                                possiblyWellKnownFinanceAgency,
+                                possiblyWellKnownBank,
+                                state.cibilRating,
+                                state.loanStatus,
+                                state.linearId).toSimpleObj()
+                    }
+
+        }
+
     }
 }
